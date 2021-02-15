74c57accbd3c07d3a8e0c619490db65d1780a6ba
chasonghui <chasonghui@naver.com>
2021-02-15 09:48:08 +0900

    PropTypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import Potato from './Potato';
+import PropTypes from "prop-types"
+function Food() {
+  return <h1>I like Food</h1>
+}
+
 function App() {
-  return <div><h1>hello!!!!</h1><Potato /></div>
+  return <div><h1>Hello</h1><Food fav="Kimchi" /></div>
 }
 
 export default App;
