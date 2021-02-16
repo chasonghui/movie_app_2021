645a94f63047e6e0bfbb52fe9e964edfefeb613f
chasonghui <chasonghui@naver.com>
2021-02-16 09:59:57 +0900

    Details

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({ movieCd, movieNm, rank, audiAcc, openDt }) {
-    return <div className="movie">
-        {/* 여기에 포스터가 있어야 하는데 .... */}
-        <div className="movie_data">
-            <h3 className="movie__title">{movieNm}</h3>
-            <h5 className="movie__rank">{rank}</h5>
-            <p className="movie__open">{openDt}</p>
-        </div>
-    </div>;
+    return (
+        <Link
+            to={{
+                pathname: "/movie-detail",
+                state: {
+                    movieCd,
+                    movieNm,
+                    rank,
+                    audiAcc,
+                    openDt
+                }
+            }}>
+            <div className="movie">
+                {/* 여기에 포스터가 있어야 하는데 .... */}
+                <div className="movie_data">
+                    <h3 className="movie__title">{movieNm}</h3>
+                    <h5 className="movie__rank">{rank}</h5>
+                    <p className="movie__open">{openDt}</p>
+                </div>
+            </div>
+        </Link>
+    );
 }
 
 Movie.propTypes = {
