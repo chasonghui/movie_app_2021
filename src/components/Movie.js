d1595ae9c576c24edca5283e0424fc9832dce01c
chasonghui <chasonghui@naver.com>
2021-02-16 10:10:40 +0900

    Detail and Styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,28 +3,28 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ movieCd, movieNm, rank, audiAcc, openDt }) {
+function Movie({ id, movieCd, movieNm, rank, audiAcc, openDt }) {
     return (
-        <Link
-            to={{
-                pathname: "/movie-detail",
-                state: {
-                    movieCd,
-                    movieNm,
-                    rank,
-                    audiAcc,
-                    openDt
-                }
-            }}>
-            <div className="movie">
+        <div className="movie">
+            <Link
+                to={{
+                    pathname: `/movie/${id}`,
+                    state: {
+                        movieCd,
+                        movieNm,
+                        rank,
+                        audiAcc,
+                        openDt
+                    }
+                }}>
                 {/* 여기에 포스터가 있어야 하는데 .... */}
                 <div className="movie_data">
                     <h3 className="movie__title">{movieNm}</h3>
                     <h5 className="movie__rank">{rank}</h5>
                     <p className="movie__open">{openDt}</p>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
     );
 }
 
